41be099f34d31df0bc6761786dfdf4d94bb89d24
vane235 <144374033+vane235@users.noreply.github.com>
2023-09-17 11:40:09 +0300

    Add files via upload

---
+++ работы в путоне — копия/кот.py
@@ -0,0 +1,21 @@
+class Cat:
+    cute = "Cat is so cute"
+    def __init__(self, name, age,color):
+        self.name = name
+        self.age = age
+        self.color = color
+tom = Cat("Tom", 2 , "black")
+simba = Cat("Simba", 3, "white")
+
+print("Tom", "age:2", "black")
+print("Simba", "age:3", "white")
+
+
+
+
+
+
+
+
+
+
