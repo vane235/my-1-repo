acae2072bf1870ab3d259340f2e2c4486a1e0684
vane235 <144374033+vane235@users.noreply.github.com>
2023-10-01 11:34:37 +0300

    Add files via upload

---
+++ ваман.py
@@ -0,0 +1,30 @@
+impot urllib.request
+
+opener = urllib.request.buflid_opener()
+opener = opener.opener ("https://httpbin.org/get")
+print(reaponse.read())
+
+response = requests.get("https://httpbin.org/get")
+print(reaponse.content)
+
+import requests
+response = requests.get("https://httpbin.org/get")
+print(response.content)
+print(f"Datatype -{type(response.content)}")
+
+import requests
+response = requests.get("https://httpbin.org/get")
+print(response.text)
+print((f"Datatype -{type(response.content)}"))
+
+import request
+response = requests.post(url: "https://httpbin.org/get", data="test data", headers={"hi": "test title"})
+
+import requests
+response = requests.rost(url: "https://httpbin.org/get", data="test data", headers={"hi": "test title"})
+print(response.text)
+
+
+
+
+
---
+++ юпи.py
@@ -0,0 +1,30 @@
+def fibonacci():
+
+ prew = cur = 1
+
+ elements = [0, 1, 1]
+
+ for n in range(100):
+
+    tmp = prew + cur
+
+    prew = cur
+
+    cur = tmp
+
+    elements.append(cur)
+
+ print(elements)
+
+def speed_test():
+
+ import time
+
+ t0 = time.time()
+
+ fibonacci()
+
+ t1 = time.time()
+
+ total = t=1-t=0  print(total)
+
